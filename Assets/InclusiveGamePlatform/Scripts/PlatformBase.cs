c13472c2676f0e47a140f5027ff7d6f2117f41c4
Ponomaryov Artyom <ponomaryovdev@gmail.com>
2022-10-01 03:55:33 +0300

    Signed-off-by: Ponomaryov Artyom <ponomaryovdev@gmail.com>

--- Assets/InclusiveGamePlatform/Scripts/PlatformBase.cs
+++ Assets/InclusiveGamePlatform/Scripts/PlatformBase.cs
@@ -1,25 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlatformBase : MonoBehaviour
 {
+    [Header("Left, Center, Right")]
+    public List<Sprite> SpriteList = new List<Sprite>();
     private GameObject _keyPrefab;
+    private GameObject _platformPart;
 
     private void Awake()
     {
         var res = Resources.LoadAsync("KeyDisplay/IM_KeyDisplay");
         _keyPrefab = (GameObject)res.asset;
+        var part = Resources.LoadAsync("UI/PlatformPart");
+        _platformPart = (GameObject)part.asset;
     }
 
     public virtual void Init(KeyBlock keyBlock)
     {
         int count = keyBlock.keys.Count;
         int index = 0;
-        LeanTween.value(0, 1, 0.25f).setOnUpdate((float val) => { }).setOnComplete(delegate() {SpawnKey(keyBlock.keys[index++]); } ).setRepeat(count);
+        LeanTween.value(0, 1, 0.15f).setOnUpdate((float val) => { }).setOnComplete(delegate() {SpawnKey(keyBlock.keys[index++]); } ).setRepeat(count);
 
 
     }
+
+    public void SpawnPlatform(int keyCount)
+    {
+        for (int i = 0; i < keyCount; i++)
+        {
+
+        }
+    }
+
     public void SpawnKey(string key)
     {
         KeyDisplayBase keyTmp = Instantiate(_keyPrefab, transform.GetChild(1).GetChild(0)).GetComponent<KeyDisplayBase>();
